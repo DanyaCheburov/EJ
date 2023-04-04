099988c5b8f2c6c54dcc7e20627e079c50f41f63
seba1rvd <kek.samp@mail.ru>
2023-04-04 16:37:34 +0300

    up

--- EJ/MainMenu/AttedancePage.xaml.cs
+++ EJ/MainMenu/AttedancePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using EJ.Properties;
 
 namespace EJ.MainMenu
 {
@@ -20,10 +22,48 @@ namespace EJ.MainMenu
     /// </summary>
     public partial class AttedancePage : Page
     {
+        public ObservableCollection<Students> Students { get; set; }
+
         public AttedancePage()
         {
             InitializeComponent();
-            ComboGroup.ItemsSource = BDEntities.GetContext().Subjects.ToList();
+            ComboSubject.ItemsSource = BDEntities.GetContext().Subjects.ToList();
+            ComboGroup.ItemsSource = BDEntities.GetContext().Students.ToList();
+            
+            using (var db = new BDEntities())
+            {
+                var students = db.Students.Include("Users").ToList();
+                Students = new ObservableCollection<Students>(students);
+            }
+
+            DataContext = this;
+
+            //Автоматическое добавление даты
+            comboBox.Items.Add(2019);
+
+            // Add all years between 2019 and the current year to the combo box
+            int currentYear = DateTime.Now.Year;
+            for (int year = 2019; year <= currentYear; year++)
+            {
+                if (!comboBox.Items.Contains(year))
+                {
+                    comboBox.Items.Add(year);
+                }
+            }
+            comboBox.SelectedItem = currentYear;
+        }
+
+        private void ComboBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            int currentMonthIndex = DateTime.Now.Month - 1;
+            ComboBox ComboMonth = sender as ComboBox;
+            ComboMonth.SelectedIndex = currentMonthIndex;
+        }
+
+        private void add_attedance_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new AddAttedance();
+            window.ShowDialog();
         }
     }
 }
