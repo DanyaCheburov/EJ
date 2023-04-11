197f3cd11ed3a51897354e61e1ef7bc9c7b5d890
seba1rvd <kek.samp@mail.ru>
2023-04-11 18:44:50 +0300

    up

--- EJ/MainMenu/AttedancePage.xaml.cs
+++ EJ/MainMenu/AttedancePage.xaml.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Navigation;
+using EJ.MainMenu;
 
 namespace EJ.MainMenu
 {
@@ -21,43 +21,6 @@ namespace EJ.MainMenu
         private int DaysInMonth;
         private DateTime dtStart;
         private DateTime dtEnd;
-
-        public class EmployeeAttendance
-        {
-            public string Name { get; set; }
-            public string StudentId { get; set; }
-            public string Day1 { get; set; }
-            public string Day2 { get; set; }
-            public string Day3 { get; set; }
-            public string Day4 { get; set; }
-            public string Day5 { get; set; }
-            public string Day6 { get; set; }
-            public string Day7 { get; set; }
-            public string Day8 { get; set; }
-            public string Day9 { get; set; }
-            public string Day10 { get; set; }
-            public string Day11 { get; set; }
-            public string Day12 { get; set; }
-            public string Day13 { get; set; }
-            public string Day14 { get; set; }
-            public string Day15 { get; set; }
-            public string Day16 { get; set; }
-            public string Day17 { get; set; }
-            public string Day18 { get; set; }
-            public string Day19 { get; set; }
-            public string Day20 { get; set; }
-            public string Day21 { get; set; }
-            public string Day22 { get; set; }
-            public string Day23 { get; set; }
-            public string Day24 { get; set; }
-            public string Day25 { get; set; }
-            public string Day26 { get; set; }
-            public string Day27 { get; set; }
-            public string Day28 { get; set; }
-            public string Day29 { get; set; }
-            public string Day30 { get; set; }
-            public string Day31 { get; set; }
-        }
 
         public AttedancePage()
         {
@@ -158,7 +121,7 @@ namespace EJ.MainMenu
 
         private void ComboGroup_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
- 
+
             LoadGrid();
         }
 
