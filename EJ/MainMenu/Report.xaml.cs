c204bd2abfc5591997f9d88c9d17e5015db5ad86
Максим <kek.samp@mail.ru>
2023-04-26 14:38:37 +0300

    up

--- EJ/MainMenu/Report.xaml.cs
+++ EJ/MainMenu/Report.xaml.cs
@@ -1,56 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Forms.DataVisualization.Charting;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace EJ.MainMenu
 {
-    /// <summary>
-    /// Логика взаимодействия для Report.xaml
-    /// </summary>
     public partial class Report : Window
     {
         private BDEntities _context = new BDEntities();
         public Report()
         {
             InitializeComponent();
-            ChartPayments.ChartAreas.Add(new ChartArea("Main"));
 
-            var currentSeries = new Series("Payments")
-            {
-                IsValueShownAsLabel = true
-            };
-            ChartPayments.Series.Add(currentSeries);
-
-            ComboUser.ItemsSource = _context.Users.ToList();
+            ComboGroup.ItemsSource = _context.Groups.ToList();
             ComboCharTypes.ItemsSource = Enum.GetValues(typeof(SeriesChartType));
         }
 
         private void UpdateChart(object sender, SelectionChangedEventArgs e)
         {
-            if (ComboUser.SelectedItem is Users currentUser &&
+            if (ComboGroup.SelectedItem is Groups currentGroup &&
                 ComboCharTypes.SelectedItem is SeriesChartType currentType)
             {
                 Series currentSeries = ChartPayments.Series.FirstOrDefault();
                 currentSeries.ChartType = currentType;
                 currentSeries.Points.Clear();
 
-                var groupsList = _context.Groups.ToList();
-                foreach (var group in groupsList)
+                var _connection = (@"Data Source=localhost\SQLEXPRESS;Initial Catalog=BD;Integrated Security=True");
+                // Выбираем студентов для определенной группы и отображаем их на графике
+                string query = "SELECT u.Name, COUNT(*) as Absences " +
+                               "FROM Users AS u " +
+                               "JOIN Students AS s ON s.UserId=u.Id " +
+                               "JOIN Groups AS g ON g.GroupId=s.GroupId " +
+                               "JOIN Attendance AS a ON a.StudentId=s.Id " +
+                               "WHERE g.GroupName = @GroupName AND a.PassType = 0 " +
+                               "GROUP BY u.Name";
+                SqlConnection connection = new SqlConnection(_connection);
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@GroupName", currentGroup.GroupName);
+
+                try
                 {
-                    currentSeries.Points.AddXY(group.GroupName,
-                        _context.Attendance.ToList().Where(a => a.StudentId == currentUser.Id
-                        && a.SubjectId == group.GroupId && a.PassType == true).Count());
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        string studentName = reader.GetString(0);
+                        double absences = Convert.ToDouble(reader.GetValue(1));
+                        currentSeries.Points.AddXY(studentName, absences);
+                    }
+                }
+                finally
+                {
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        connection.Close();
+                    }
                 }
             }
         }
