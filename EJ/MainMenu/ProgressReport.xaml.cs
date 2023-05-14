8c303bbf461da064051a42ea7fc23ac7afc94347
DanyaCheburov <chebur.danya@gmail.com>
2023-05-14 15:48:21 +0300

    Добавлен гарфик оценок

--- EJ/MainMenu/ProgressReport.xaml.cs
+++ EJ/MainMenu/ProgressReport.xaml.cs
@@ -1,17 +1,17 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Drawing.Charts;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Drawing;
 using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace EJ.MainMenu
 {
@@ -25,10 +25,11 @@ namespace EJ.MainMenu
         public string SelectedSubject { get; set; }
         public int SelectedYear { get; set; }
         public int SelectedMonth { get; set; }
+        
         public ProgressReport(string selectedGroup, string selectedSubject, int selectedYear, int selectedMonth)
         {
             InitializeComponent();
-            ComboStudents.ItemsSource=_context.Users.ToList();
+            
             SelectedYear = selectedYear;
             SelectedGroup = selectedGroup;
             ComboGroup.ItemsSource = _context.Groups.ToList();
@@ -43,6 +44,18 @@ namespace EJ.MainMenu
             ComboMonth.ItemsSource = CultureInfo.CurrentCulture.DateTimeFormat.MonthNames.Take(12).ToList();
             ComboMonth.SelectedItem = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(selectedMonth + 1);
 
+        }
+        private IEnumerable<Students> Students
+        {
+            get
+            {
+                if (ComboGroup.SelectedItem != null)
+                {
+                    var selectedGroup = ComboGroup.SelectedItem as Groups;
+                    return BDEntities.GetContext().Students.Where(s => s.GroupId == selectedGroup.GroupId).ToList();
+                }
+                return null;
+            }
         }
         private void BtnExit_Click(object sender, RoutedEventArgs e)
         {
@@ -62,6 +75,69 @@ namespace EJ.MainMenu
         }
         private void UpdateChart(object sender, RoutedEventArgs e)
         {
+            ComboStudents.ItemsSource = Students;
+            if (ComboGroup.SelectedItem is Groups currentGroup &&
+                 ComboSubject.SelectedItem is Subjects currentSubject &&
+                 ComboMonth.SelectedItem is string selectedMonthName &&
+                 ComboStudents.SelectedItem is Students currentStudent) 
+            {
+                ChartsEtimates.Series.Clear();
+                Series currentSeries = new Series
+                {
+                    Name = "",
+                    ChartType = SeriesChartType.Pie
+                };
+                var selectedGroup = ComboGroup.SelectedItem as Groups;
+                var selectedSubject = ComboSubject.SelectedItem as Subjects;
+                var selectedMonth = ComboMonth.SelectedIndex + 1;
+                var selectedYear = Convert.ToInt32(ComboYear.SelectedItem);
+
+                  
+                using (var db = new BDEntities())
+                {
+                    var query = from j in _context.Journal
+                                  join sb in _context.Subjects on j.SubjectId equals sb.SubjectId
+                                  join st in _context.Students on j.StudentId equals st.StudentId
+                                  join u in _context.Users on st.UserId equals u.UserId
+                                  join g in _context.Groups on st.GroupId equals g.GroupId
+                                  where g.GroupName == selectedGroup.GroupName
+                                      && sb.SubjectName == selectedSubject.SubjectName
+                                      && j.Date.Month == selectedMonth
+                                      && j.Date.Year == selectedYear
+                                  select new
+                                  {
+                                      GroupName = g.GroupName,
+                                      UserName = u.UserName,
+                                      SubjectName = sb.SubjectName,
+                                      Estimate = j.Estimate,
+                                      Date = j.Date
+                                  };
+                    var grades = query.ToList();
+                    Dictionary<string, int> gradesCount = new Dictionary<string, int>();
+                    foreach (var grade in grades)
+                    {
+                        if (gradesCount.ContainsKey(grade.Estimate.ToString()))
+                        {
+                            gradesCount[grade.Estimate.ToString()]++;
+                        }
+                        else
+                        {
+                            gradesCount.Add(grade.Estimate.ToString(), 1);
+                        }
+                        
+                    }
+                    
+                    foreach (var item in gradesCount)
+                    {
+                        double percentage = (double)item.Value / grades.Count * 100;
+                        string label = $"{percentage}%";
+                        currentSeries.Points.AddXY(label, item.Value);
+                    }
+                    ChartsEtimates.Series.Clear();
+                    ChartsEtimates.Series.Add(currentSeries);
+                }
+
+            }
 
         }
     }
