fd5fb4841203b774f1535a6ac5f8b3244a960274
DanyaCheburov <chebur.danya@gmail.com>
2023-05-22 11:24:29 +0300

    Редактирован экспорт в Word успеваемости

---
+++ EJ/MainMenu/EstimateEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ.MainMenu
+{
+    public class EstimateEntry
+    {
+        public string UserName { get; set; }
+        public string GroupName { get; set; }
+        public string SubjectName { get; set; }
+        public DateTime Date { get; set; }
+        public int Estimate { get; set; }
+        public DateTime LessonDate { get; set; }
+        public string Description { get; set; }
+    }
+}
