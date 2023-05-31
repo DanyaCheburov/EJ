17507988c7deb775cd667084bc131d45b26db50f
seba1rvd <kek.samp@mail.ru>
2023-05-31 20:48:50 +0300

    update

--- EJ/MainMenu/AttendanceStudentPage.xaml.cs
+++ EJ/MainMenu/AttendanceStudentPage.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 
 namespace EJ.MainMenu
 {
@@ -180,5 +183,124 @@ namespace EJ.MainMenu
 
             return -1; // Если не удалось получить идентификатор студента, возвращаем значение по умолчанию
         }
+
+        private void ToCreatePDF_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime startDate = StartOfPeriod.SelectedDate ?? DateTime.MinValue;
+            DateTime endDate = EndOfPeriod.SelectedDate ?? DateTime.MaxValue;
+
+            int studentId = GetStudentId();
+
+            var query = from attendance in db.Attendance
+                        join student in db.Students on attendance.StudentId equals student.StudentId
+                        join subject in db.Subjects on attendance.SubjectId equals subject.SubjectId
+                        where student.StudentId == studentId &&
+                              attendance.Date >= startDate && attendance.Date <= endDate
+                        select new
+                        {
+                            subject.SubjectName,
+                            attendance.Date,
+                            attendance.PassType
+                        };
+
+            var reportData = query.ToList();
+
+            // Создание списка уникальных дат и сортировка их по возрастанию
+            var uniqueDates = reportData.Select(item => item.Date.Date).Distinct().OrderBy(date => date).ToList();
+
+            // Создание списка объектов AttendanceReportItem
+            List<AttendanceReportItem> reportItems = new List<AttendanceReportItem>();
+
+            foreach (var item in reportData)
+            {
+                string passType = item.PassType ? "УП" : "Н";
+
+                // Поиск соответствующего объекта AttendanceReportItem для предмета
+                AttendanceReportItem reportItem = reportItems.FirstOrDefault(r => r.SubjectName == item.SubjectName);
+
+                if (reportItem == null)
+                {
+                    reportItem = new AttendanceReportItem
+                    {
+                        SubjectName = item.SubjectName
+                    };
+                    reportItems.Add(reportItem);
+                }
+
+                // Заполнение данных о пропусках для каждой даты
+                if (!reportItem.DateData.ContainsKey(item.Date.Date))
+                {
+                    reportItem.DateData.Add(item.Date.Date, passType);
+
+                    if (passType == "УП")
+                    {
+                        reportItem.UPCount++;
+                    }
+                    else if (passType == "Н")
+                    {
+                        reportItem.NCount++;
+                    }
+                }
+            }
+
+            // Создание документа PDF
+            Document document = new Document();
+            string fileName = "AttendanceReport.pdf";
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), fileName);
+
+            try
+            {
+                PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(filePath, FileMode.Create));
+                document.Open();
+
+                // Добавление заголовка
+                Paragraph title = new Paragraph("Attendance Report");
+                title.Alignment = Element.ALIGN_CENTER;
+                title.Font = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 20f);
+                document.Add(title);
+
+                // Добавление данных таблицы
+                PdfPTable table = new PdfPTable(uniqueDates.Count + 3);
+                table.WidthPercentage = 100;
+
+                // Добавление столбцов таблицы
+                table.AddCell("Предмет");
+                foreach (var date in uniqueDates)
+                {
+                    table.AddCell(date.ToString("dd.MM.yy"));
+                }
+                table.AddCell("Отсутствие по уважительной причине");
+                table.AddCell("Отсутствие по неуважительной причине");
+
+                // Добавление строк и данных таблицы
+                foreach (AttendanceReportItem reportItem in reportItems)
+                {
+                    table.AddCell(reportItem.SubjectName); // Добавление ячейки с названием предмета
+
+                    foreach (var date in uniqueDates)
+                    {
+                        string passType = reportItem.DateData.ContainsKey(date) ? reportItem.DateData[date] : "";
+                        table.AddCell(passType); // Добавление ячейки с типом пропуска для каждой даты
+                    }
+
+                    table.AddCell(reportItem.UPCount.ToString()); // Добавление ячейки с количеством уважительных пропусков
+                    table.AddCell(reportItem.NCount.ToString()); // Добавление ячейки с количеством неуважительных пропусков
+                }
+
+                document.Add(table);
+
+                // Закрытие документа
+                document.Close();
+
+                MessageBox.Show("Отчет успешно создан и сохранен в документе " + fileName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при создании отчета: " + ex.Message);
+            }
+        }
+
+
+
     }
 }
